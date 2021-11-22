d1c631b470fb3ac9ce3d4bfcd9f83bb606546246
yash-2115 <mailforyash04@gmail.com>
2021-11-22 13:39:54 +0530

    __name__ == __main__

---
+++ 02 name and main/01 name is equal to main.py
@@ -0,0 +1,7 @@
+def caclulator(base, height):
+    return 1/2*(base/height)
+
+# This if loop is used as an entry point of the python program
+if __name__ == "__main__":
+    a = caclulator(10, 20)
+    print("Area: ", a)
